7b498dead1c1b37f2f61a210a888559b6163ce28
pontus <pontus@fastmail.org>
2026-03-26 13:16:04 +0100

    feat: add texture & framebuffer

--- src/main.zig
+++ src/main.zig
@@ -16,16 +16,19 @@ const screen_title: [*c]const u8 = "working-title";
 const SdlContext = struct {
     window: *c.SDL_Window,
     renderer: *c.SDL_Renderer,
+    texture: *c.SDL_Texture,
 
     pub fn deinit(self: SdlContext) void {
         c.SDL_DestroyRenderer(self.renderer);
         c.SDL_DestroyWindow(self.window);
+        c.SDL_DestroyTexture(self.texture);
     }
 };
 
 fn initSdl() !SdlContext {
     var window: ?*c.SDL_Window = null;
     var renderer: ?*c.SDL_Renderer = null;
+    var texture: ?*c.SDL_Texture = null;
 
     if (!c.SDL_Init(c.SDL_INIT_VIDEO)) {
         std.debug.print("SDL_Init failed: {s}\n", .{c.SDL_GetError()});
@@ -38,11 +41,15 @@ fn initSdl() !SdlContext {
         return error.SdlWindowCreationFailed;
     }
 
+    texture = c.SDL_CreateTexture(renderer, c.SDL_PIXELFORMAT_RGBA8888, c.SDL_TEXTUREACCESS_STREAMING, screen_width, screen_height);
+
+    if (texture == null) {
+        std.debug.print("SDL_CreateTexture failed: {s}\n", .{c.SDL_GetError()});
+        return error.SdlCreateTextureFailed;
+    }
+
     // We can unwrap safely because of the assertion above
-    return SdlContext{
-        .window = window.?,
-        .renderer = renderer.?,
-    };
+    return SdlContext{ .window = window.?, .renderer = renderer.?, .texture = texture.? };
 }
 
 const Point2D = struct {
@@ -95,6 +102,9 @@ pub fn main() !void {
     var is_running: bool = true;
     var event: c.SDL_Event = undefined;
 
+    var pixels: ?*anyopaque = null;
+    var pitch: c_int = 0;
+
     // TODO: This is kinda stupid, most of these can throw errors
     // so we probably want to look into what to handle explicitly
     while (is_running) {
@@ -104,8 +114,18 @@ pub fn main() !void {
             }
         }
 
-        _ = c.SDL_SetRenderDrawColor(sdl_context.renderer, 255, 255, 255, 255);
-        _ = c.SDL_RenderClear(sdl_context.renderer);
+        // pixels is the pointer to the raw memory where the pixels live
+        // pitch is the number of bytes PER ROW so we know how many steps to take
+        _ = c.SDL_LockTexture(sdl_context.texture, null, &pixels, &pitch);
+
+        // This allows us to index into the pixel array and modify it
+        const pixel_data: [*]u32 = @ptrCast(@alignCast(pixels.?));
+        const stride = @divExact(@as(usize, @intCast(pitch)), 4);
+        @memset(pixel_data[0 .. stride * screen_height], 0);
+
+        // Draw
+        _ = c.SDL_UnlockTexture(sdl_context.texture);
+        _ = c.SDL_RenderTexture(sdl_context.renderer, sdl_context.texture, null, null);
         _ = c.SDL_RenderPresent(sdl_context.renderer);
     }
 }
